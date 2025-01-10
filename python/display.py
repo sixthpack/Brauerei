9e862ecb73cb09574d24e6ba44f00e76ba2ade92
Paul Schütz <paul.schuetz@gmx.de>
2025-01-10 14:47:16 +0100

    Auf test Werte umgestellt und Display umgestellt auf window

--- python/display.py
+++ python/display.py
@@ -5,47 +5,53 @@ import curses, time
 
 class Display:
     
-    global sensor1, sensor2, stdscr
+    global sensor1, sensor2, sensorWindow
 
-    def __init__(self, stdscr):
-        self.stdscr = stdscr
-
-    def repaint(self):
-        # stdscr = curses.initscr()
+    def __init__(self):
+        height, width = 14, 15
+        start_y, start_x = 2, 2
+        self.sensorWindow = curses.newwin(height, width, start_y, start_x)
+        
+    def repaintSensors(self):
+        #.sensorWindow = curses.initscr()
         # curses.curs_set(False)
         curses.start_color()
         curses.init_pair( 1, curses.COLOR_BLACK, curses.COLOR_RED)
         curses.init_pair( 2, curses.COLOR_BLACK, curses.COLOR_WHITE)
-        x1 = int ( (curses.COLS - 5 )/2)
-        x2 = int ( (curses.COLS + 5 )/2)
+        curses.init_pair( 3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+        BLACK_AND_RED = curses.color_pair(1)
+        BLACK_AND_WHITE = curses.color_pair(2)
+        WHITE_AND_BLACK = curses.color_pair(3)
 
-        self.stdscr.clear()
-        self.stdscr.refresh()
-        #temp1 = self.sensor1#.split('.')[0]
-        #temp2 = self.sensor2#.split('.')[0]
-        self.stdscr.addstr(4, x1, str(self.sensor1)+"°C", curses.A_BOLD)
-        self.stdscr.addstr(4, x2, str(self.sensor2)+"°C", curses.A_BOLD)
+        x1 = 1
+        x2 = 8
+
+        self.sensorWindow.clear()
+        self.sensorWindow.refresh()
         
-        #v1 = int(temp1)
-        #v2 = int(temp2)
+        self.sensorWindow.addstr(1, 2, "Temperatur:", curses.A_BOLD)
+        self.sensorWindow.addstr(2, x1, str(self.sensor1)+"°C", curses.A_BOLD)
+        self.sensorWindow.addstr(2, x2, str(self.sensor2)+"°C", curses.A_BOLD)
         
         for i in range(10):
-            for j in range(4):
+            for j in range(6):
                 if i*10 < 100-self.sensor1:
-                    self.stdscr.addstr(5+i, x1+j, " ", curses.color_pair(2))
+                    self.sensorWindow.addstr(i+3, x1+j, " ", BLACK_AND_WHITE)
                 else:
-                    self.stdscr.addstr(5+i, x1+j, " ", curses.color_pair(1))
+                    self.sensorWindow.addstr(i+3, x1+j, " ", BLACK_AND_RED)
 
         for i in range(10):
-            for j in range(4):
+            for j in range(6):
                 if i*10 < 100-self.sensor2:
-                    self.stdscr.addstr(5+i, x2+j, " ", curses.color_pair(2))
+                    self.sensorWindow.addstr(i+3, x2+j, " ", BLACK_AND_WHITE)
                 else:
-                    self.stdscr.addstr(5+i, x2+j, " ", curses.color_pair(1))
+                    self.sensorWindow.addstr(i+3, x2+j, " ", BLACK_AND_RED)
+        
 
-        self.stdscr.refresh()
+        self.sensorWindow.box()
+        self.sensorWindow.refresh()
 
     def setSensorValues(self, s1, s2):
-        self.sensor1 = int(s1)#s1.split('.')[0]
-        self.sensor2 = int(s2)#s2.split('.')[0]
-        self.repaint();
+        self.sensor1 = s1
+        self.sensor2 = s2
+        self.repaintSensors();
